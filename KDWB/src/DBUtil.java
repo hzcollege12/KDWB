eba827f40ea3157c4cb0a8685e6746914336defb
st01 <st01@teh>
2014-11-07 15:57:20 +0800

    kehua 2014/11/7

--- KDWB/src/DBUtil.java
+++ KDWB/src/DBUtil.java
@@ -11,10 +11,10 @@ public static String registerUser(String u_name,String u_pwd,String u_email,Stri
 			result=CONNECTION_OUT;
 			return result;
 		}
-		ps=com.prepareStatement("insert into user(u_no,u_name,u_pwd,u_email,u_state,h_id)"+"values(?,?,?,?,?,?)")
+		ps=com.prepareStatement("insert into user(u_no,u_name,u_pwd,u_email,u_state,h_id)"+"values(?,?,?,?,?,?)");
 		String u_no=String.valueOf(getMax(USER));
 		u_name=new String(u_name.getBytes(),"ISO-8859-1");
-		u_state=new String(u_state.getBytes(),"ISO-8859-1")'
+		u_state=new String(u_state.getBytes(),"ISO-8859-1");
 		int no=Integer.valueOf(u_no);
 		int hid=Integer.valueOf(h_id);
 		ps.setInt(1,no);
@@ -30,9 +30,12 @@ public static String registerUser(String u_name,String u_pwd,String u_email,Stri
 		else{
 			result=REGISTER_FAIL;
 			}
-		}catch(Exception e){e.printStackTrace();}
-		finally{
-			tey{if(ps!=null){ps.close();ps=null;}
+		}catch(Exception e)	{
+			e.printStackTrace();
+		}finally{
+			try{if(ps!=null){
+				ps.close();ps=null;
+			}
 			}catch(Exception e){e.printStacktrace();}
 			try{if(con!=null){con.close();con=null;}
 			}catch(Exception e){e.printStackTrace();}
