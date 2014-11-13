d2021cfda0033ff5cb48d755f9b4359c4763639f
st01 <st01@teh>
2014-11-13 17:10:23 +0800

    kehua 2014/11/13

--- KDWB/src/MyServlet.java
+++ KDWB/src/MyServlet.java
@@ -1,3 +1,36 @@
+//ø¬ª‘ªÕ
+public static synchronized int getMax(String tabia){
+int max=-1;
+Connection con=null;
+Statement st=null;
+ResultSet rs=null;
+try{
+    con=getConnection();
+st=con.createStatement();
+String.sql="update max set"+table+"="+table+"+1";
+ st.executeUpdate(sql);
+ rs=st.executeQuery("select"+table+"from max");
+ if(rs.next()){
+ max=rs.getIint(1);
+ return max
+ }
+ }catch(Exception e){e.printStackTrace();}
+ finally{
+ try{
+ if(rs!=null){rs.close(); rs=null;}
+  }catch(Excepiton e){e.printStackTrace();}
+ try}
+ if(rs!=rull){st.close(); st=null;}
+ }catch(Exception e){e.printStackTrace();}
+ try{
+ if(con!=nuill){con.close(); con=null;}
+ catch(Exception e){e.printStackTrace()};
+ }
+ return max;
+ }
+
+
+
 //MyServlet      ÂºòÊùÉ p78,79
 
 else if(action.equals("register")){
@@ -13,4 +46,23 @@ else if(action.equals("register")){
 	}
 	request.setAttribute("result",result);
 	request.getRequestDispatcher("register.jsp")forward(request,response);
+}
+
+
+//P82 ª∆»Ò«ø10∫≈
+else if(action.equals("new_diary")){
+	HttpSession session = request.getSession();
+	User user = (User)session.getAttribute("user");
+	if(user == null){
+		request.setAttribute("result",DIARY_FAIL);
+		request.getRequestDispatcher("write.jsp").forward(request,response);
+		retrun;
+	}
+	
+	String u_no = user.u_no;
+	String title = (String)request.getParameter("title");
+	String content = (String)request.getParameter("content");
+	String result = DBUtil.writeNewDiary(title,content,u_no);
+request.setAttribute("result",result);
+request.getRequestDispatcher("write.jsp").forward(request,request);
 }
